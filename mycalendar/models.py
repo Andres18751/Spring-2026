2d850ce80dbb743a5c610159277c3f4ed8c1ac21
Andres18751 <91837600+Andres18751@users.noreply.github.com>
2026-04-22 04:47:46 +0000

    Additions to profile

--- mycalendar/models.py
+++ mycalendar/models.py
@@ -3,6 +3,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+TAGS_CHOICES = [
+    ('ssbu','Super Smash Bros. Ultimate'),
+    ('sf6','Street Fighter 6'),
+    ('ow','Overwatch'),
+    ('mk','Mortal Kombat'),
+    ('dbfz','Dragon Ball Fighterz'),
+]
+
+MEETING_TIME_CHOICES = [
+    ('morning', 'Morning (8 AM - 11 AM)'),
+    ('afternoon', ' Afternoon (12 PM - 4 PM)'),
+    ('evening', ' Evening (4 PM - 8 PM)'),
+]
+
+DAYS_AVAILABLE =[
+    ('mon','Monday'),
+    ('tue','Tuesday'),
+    ('wed','Wednesday'),
+    ('thu''Thursday'),
+    ('fri','Friday')
+]
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -19,6 +41,20 @@ class Profile(models.Model):
     # The fields they can fill out
     bio = models.TextField(max_length=500, blank=True)
     favorite_games = models.CharField(max_length=100, blank=True)
+
+    tags = models.CharField(
+        max_length=200, 
+        blank=True, 
+    )
+    
+    meeting_times = models.CharField(
+        max_length=200, 
+        blank=True, 
+    )
+    days_available = models.CharField(
+        max_length=200, 
+        blank=True, 
+    )
 
     def __str__(self):
         return f"{self.user.username}'s Loadout"
