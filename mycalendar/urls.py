4c722823d1eeb6f39afffd57b12ba9dd52b90b55
Andres18751 <91837600+Andres18751@users.noreply.github.com>
2026-04-29 19:09:19 +0000

    login html

--- mycalendar/urls.py
+++ mycalendar/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.show_calendar, name='calendar-home'),
     path('<int:year>/<int:month>/', views.show_calendar, name='calendar-specific'),
-    
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='mycalendar/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='mycalendar/login.html'), name='login'),
     # NEW PATH: Routes traffic when a specific day is clicked
     path('<int:year>/<int:month>/<int:day>/', views.daily_events, name='daily-events'),
 ]
